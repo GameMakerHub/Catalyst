d68474f8050cb916ee206ea7bf701165413bd64e
RobQuistNL <rob.quist@dukesoft.nl>
2019-11-05 12:56:08 +0100

    Update test to actually do something

--- tests/src/Service/CleanServiceTest.php
+++ tests/src/Service/CleanServiceTest.php
@@ -2,7 +2,6 @@
 
 namespace Catalyst\Tests\Service;
 
-use Catalyst\Entity\CatalystEntity;
 use Catalyst\Service\CatalystService;
 use Catalyst\Service\CleanService;
 use Catalyst\Service\PackageService;
@@ -23,6 +22,7 @@ class CleanServiceTest extends \PHPUnit\Framework\TestCase
     protected function setUp() : void
     {
         $this->storageService = \Mockery::mock(StorageService::class . '[persist]');
+        StorageService::setInstance($this->storageService);
         $this->packageService = \Mockery::mock(PackageService::class);
         $this->subject = new CleanService($this->packageService);
     }
@@ -30,19 +30,55 @@ class CleanServiceTest extends \PHPUnit\Framework\TestCase
     public function testProjectClean()
     {
         // Load the actual file, then overwrite the storage service once files are going to be changed;
-        $catalystProject = (new CatalystService())->load(__DIR__ . '/../../mocks/projects/21_UninstalledNewFiles');
-        StorageService::setInstance($this->storageService);
-
-        $this->assertCount(0, $GLOBALS['storage']['writes']);
-        $this->assertCount(0, $GLOBALS['storage']['deletes']);
-        $this->assertCount(0, $catalystProject->ignored());
+        $projectPath = __DIR__ . '/../../mocks/projects/21_UninstalledNewFiles';
+        chdir($projectPath);
+        $catalystProject = (new CatalystService())->load('.');
 
         // Actual testing
         $this->subject->clean($catalystProject);
 
         $this->assertCount(15, $GLOBALS['storage']['writes']);
-        $this->assertCount(0, $GLOBALS['storage']['deletes']);
+        $this->assertCount(106, $GLOBALS['storage']['deletes']);
         $this->assertCount(0, $catalystProject->ignored());
+
+        $filesThatShouldNotBeDeleted = [
+            'objects/obj_test',
+            'objects/obj_test/obj_test.yy',
+            'scripts/newScript',
+            'scripts/newScript/newScript.gml',
+            'scripts/newScript/newScript.yy',
+            'sprites/newSprite',
+            'sprites/newSprite/newSprite.yy',
+            'sprites/newSprite/e131be19-5848-45f2-b020-57f4410cf2a5.png',
+            'sprites/newSprite/layers',
+            'sprites/newSprite/layers/e131be19-5848-45f2-b020-57f4410cf2a5',
+            'sprites/newSprite/layers/353bdc59-fc95-4c0e-b2e1-3c518555cc48/1fb31644-396c-40ab-95b2-00d06ba69873.png',
+            'objects',
+            'objects/obj_test',
+            'objects/obj_test/obj_test.yy',
+        ];
+
+        $filesThatShouldBeDeleted = [
+            'objects/ExtendedFunctions_obj_near_instance',
+            'objects/ExtendedFunctions_obj_test',
+            'scripts/extract_header_from_string',
+            'scripts/hextodec/hextodec.gml',
+            'scripts/hextodec/hextodec.yy',
+        ];
+
+        foreach ($filesThatShouldBeDeleted as $file) {
+            $this->assertArrayHasKey(
+                StorageService::pathToAbsolute($projectPath . '/' . $file),
+                $GLOBALS['storage']['deletes']
+            );
+        }
+
+        foreach ($filesThatShouldNotBeDeleted as $file) {
+            $this->assertArrayNotHasKey(
+                StorageService::pathToAbsolute($projectPath . '/' . $file),
+                $GLOBALS['storage']['deletes']
+            );
+        }
     }
 
     protected function tearDown() : void {
